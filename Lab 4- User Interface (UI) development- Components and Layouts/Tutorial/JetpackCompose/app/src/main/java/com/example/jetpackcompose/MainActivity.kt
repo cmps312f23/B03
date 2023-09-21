f983ddcadd70e7cb8075ca831209d24bbbc35003
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-09-21 15:26:31 +0300

    updated code

---
+++ Lab 4- User Interface (UI) development- Components and Layouts/Tutorial/JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                    MyApp(Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+
+    var names = (1..20).map { "Name $it" }
+
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        LazyColumn{
+            items(names){
+                Greeting(it)
+            }
+        }
+//       Column {
+//           names.forEach {
+//               Greeting(it)
+//           }
+//       }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary , modifier = modifier.padding(vertical = 4.dp , horizontal = 8.dp)) {
+        Text(text = "Hello Mr. $name!", modifier = modifier)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MyAppPreview() {
+    JetpackComposeTheme {
+        MyApp()
+    }
+}
