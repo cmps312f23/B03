93e19c3a28c373111e7663115722d6019bdb1267
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-09-26 15:39:46 +0300

    lab 5

--- Lab 4- User Interface (UI) development- Components and Layouts/Tutorial/JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ Lab 4- User Interface (UI) development- Components and Layouts/Tutorial/JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,16 +5,24 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,6 +35,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -53,30 +65,33 @@ fun MyApp(modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.background,
         modifier = modifier
     ) {
-        if (showOnboarding)
-             OnboardingScreen(){
-                 showOnboarding = !showOnboarding
-             }
-        else
-            LazyColumn {
-                items(names) {
-                    Greeting(it)
-                }
+//        if (showOnboarding)
+//             OnboardingScreen(){
+//                 showOnboarding = !showOnboarding
+//             }
+//        else
+        LazyColumn {
+            items(names) {
+                Greeting(it)
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun OnboardingScreen(modifier: Modifier = Modifier , changeShowOnBoard: (Boolean) -> Unit = {}) {
-    Surface {
-       Column(verticalArrangement = Arrangement.Center,
-           horizontalAlignment = Alignment.CenterHorizontally) {
-           Text(text = "Welcome to Jetpack Compose", modifier = modifier)
-           ElevatedButton(onClick = { changeShowOnBoard(false) }) {
-               Text(text = "Continue")
-           }
-       }
+fun OnboardingScreen(modifier: Modifier = Modifier, changeShowOnBoard: (Boolean) -> Unit = {}) {
+    Card {
+        Column(
+            modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to Jetpack Compose", modifier = modifier)
+            ElevatedButton(onClick = { changeShowOnBoard(false) }) {
+                Text(text = "Continue")
+            }
+        }
+
     }
 }
 
@@ -85,29 +100,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     var expanded by rememberSaveable { mutableStateOf(false) }
 
     Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp)
-            .fillMaxWidth()
+        color = MaterialTheme.colorScheme.background,
     ) {
-        Row {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(10.dp)
-            ) {
-                Text(
-                    text = "Hello Mr. $name!",
-                    modifier = modifier.padding(bottom = 10.dp)
-                )
-                if (expanded)
-                    Text(text = "Bio : This person was born in the year of bla bla bla and he gradualted from bla blu bla")
-            }
-            ElevatedButton(onClick = {
-                expanded = !expanded
-                Log.d("ButtonInfo", "I am clicked from : $name , value of expanded = $expanded")
-            }, modifier = modifier.padding(24.dp)) {
-                Text(text = if (expanded) "Show Less" else "Show More")
+        Card(
+            modifier = modifier
+                .padding(vertical = 4.dp, horizontal = 8.dp)
+                .fillMaxWidth().animateContentSize()
+        ) {
+            Row {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(10.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.person),
+                        contentDescription = "Person Image",
+                        modifier
+                            .size(100.dp)
+                            .clip(CircleShape)
+                            .border(1.5.dp , MaterialTheme.colorScheme.background, shape = CircleShape)
+                    )
+                    Text(
+                        text = "Hello Mr. $name!",
+                        modifier = modifier.padding(bottom = 10.dp)
+                    )
+                    if (expanded)
+                        Text(text = "Bio : This person was born in the year of bla bla bla and he gradualted from bla blu bla",
+                            )
+                }
+                Button(onClick = {
+                    expanded = !expanded
+                    Log.d("ButtonInfo", "I am clicked from : $name , value of expanded = $expanded")
+                }, modifier = modifier.padding(24.dp)
+                    .align(Alignment.CenterVertically)) {
+                    Text(text = if (expanded) "Show Less" else "Show More")
+                }
             }
         }
     }
