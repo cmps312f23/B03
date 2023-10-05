4a99d7d7d206fd39b5a8b69eeb1537dd0bc2240f
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-05 15:20:14 +0300

    stadium list

---
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/ui/StadiumList.kt
@@ -0,0 +1,17 @@
+package com.example.stadiums.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.stadiums.model.Stadium
+
+@Composable
+fun StadiumCard(stadium : Stadium){
+    Text(text = "Hello")
+}
+
+@Preview
+@Composable
+fun StadiumCardPreview(){
+
+}
