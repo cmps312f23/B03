306b0b4a80da472ab349f392c93168809be5e5e9
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-05 15:24:43 +0300

    stadium list

--- Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/ui/StadiumList.kt
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/ui/StadiumList.kt
@@ -4,14 +4,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stadiums.model.Stadium
+import com.example.stadiums.ui.theme.StadiumsTheme
 
 @Composable
 fun StadiumCard(stadium : Stadium){
-    Text(text = "Hello")
+    Text(text = stadium.name)
 }
 
 @Preview
 @Composable
 fun StadiumCardPreview(){
+    val stadium = Stadium("Lusail City Stadium",
+        "Doha", "Opened" ,
+        seatingCapacity = 10000 ,
+        imageName = "lusail_city")
 
+    StadiumsTheme {
+        StadiumCard(stadium)
+    }
 }
