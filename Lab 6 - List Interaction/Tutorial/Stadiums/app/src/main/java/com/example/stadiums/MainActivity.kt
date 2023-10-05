afe5c1b09acc434541dd5f6240f3ef98b09ede6b
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-05 15:10:04 +0300

    repo update

--- Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/MainActivity.kt
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.stadiums
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -34,4 +36,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
     var stadiums = StadiumRepo.getStadiums(LocalContext.current)
+    Column {
+        stadiums.forEach {
+            Text(text = it.name)
+        }
+    }
 }
