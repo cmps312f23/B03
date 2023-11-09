5a70c892e000232d5a6b3dee2ed366c598ddbf6a
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-11-09 16:39:52 +0300

    updated

--- Lab 9-Web API with Coroutines/Tutorial/BankingApp/app/src/main/java/com/cmps312/bankingapp/data/webapi/QuBankService.kt
+++ Lab 9-Web API with Coroutines/Tutorial/BankingApp/app/src/main/java/com/cmps312/bankingapp/data/webapi/QuBankService.kt
@@ -5,8 +5,14 @@ import com.cmps312.bankingapp.data.model.BankService
 import com.cmps312.bankingapp.data.model.Beneficiary
 import com.cmps312.bankingapp.data.model.Transfer
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -30,11 +36,17 @@ class QuBankService : BankService {
     }
 
     override suspend fun getTransfers(cid: Int): List<Transfer> {
-        TODO("Not yet implemented")
+        val url = "$baseUrl/transfers/$cid"
+        return client.get(url).body()
+
     }
 
     override suspend fun addTransfer(transfer: Transfer): Transfer {
-        TODO("Not yet implemented")
+        val url = "$baseUrl/transfers/${transfer.cid}"
+        return client.post(url){
+            setBody(transfer)
+            contentType(ContentType.Application.Json)
+        }.body()
     }
 
     override suspend fun deleteTransfer(cid: Int, transferId: String): String {
