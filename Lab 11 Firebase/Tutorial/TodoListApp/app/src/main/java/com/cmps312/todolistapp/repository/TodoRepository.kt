4ea0c4312e6b320dc961bb35544d4882ebe78574
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-11-23 15:47:01 +0300

    updated code

--- Lab 11 Firebase/Tutorial/TodoListApp/app/src/main/java/com/cmps312/todolistapp/repository/TodoRepository.kt
+++ Lab 11 Firebase/Tutorial/TodoListApp/app/src/main/java/com/cmps312/todolistapp/repository/TodoRepository.kt
@@ -9,25 +9,74 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 
+//All the crud operations
 class TodoRepository {
+    val TAG = "TodoRepository"
 
+    //get a link to the database
+    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    fun observeProjects(): Flow<List<Project>>
+    // get the collections
+    private val projectCollectionRef = db.collection("project")
+    private val todosCollectionRef = db.collection("todos")
 
-    fun addProject(project: Project)
+    fun addProject(project: Project) = projectCollectionRef.add(project)
+    fun updateProject(project: Project) = projectCollectionRef
+        .document(project.id)
+        .set(project)
 
-    fun updateProject(project: Project)
+    //    come back to this function
+    suspend fun deleteProject(project: Project) {
+//        delete all the todos
+        val todos = getTodoListByProject(project.id)
+        todos.forEach { deleteTodo(it) }
 
-    suspend fun deleteProject(project: Project)
+        //delete the project
+        projectCollectionRef.document(project.id).delete()
+    }
 
-    fun observeTodos(pid: String): Flow<List<Todo>>
+    fun observeProjects(): Flow<List<Project>> = callbackFlow {
+        val snapShotListener = projectCollectionRef.addSnapshotListener { values, err ->
+            if (err != null)
+                return@addSnapshotListener
+            //parse the values
+            val projects = values!!.toObjects(Project::class.java)
+            trySend(projects)
+        }
+        awaitClose { snapShotListener.remove() }
+    }
 
-    suspend fun getTodo(id: String):
-    suspend fun addTodo(todo: Todo)
+    fun observeTodos(pid: String): Flow<List<Todo>> = callbackFlow {
+        val snapShotListener = todosCollectionRef
+            .whereEqualTo("pid", pid)
+            .addSnapshotListener { values, err ->
+                if (err != null)
+                    return@addSnapshotListener
+                //parse the values
+                val todos = values!!.toObjects(Todo::class.java)
+                trySend(todos)
+            }
+        awaitClose { snapShotListener.remove() }
+    }
 
+    suspend fun getTodo(id: String) =
+        todosCollectionRef
+            .document(id)
+            .get()
+            .await()
+            .toObject(Todo::class.java)
 
-    fun updateTodo(todo: Todo)
-    fun deleteTodo(todo: Todo)
+    suspend fun addTodo(todo: Todo) = todosCollectionRef.add(todo).await().id
 
-    suspend fun getTodoListByProject(pid: String)
+    fun updateTodo(todo: Todo) = todosCollectionRef
+        .document(todo.id)
+        .set(todo)
+        .addOnSuccessListener { Log.d(TAG, "Successfully updated") }
+        .addOnFailureListener { Log.d(TAG, "Unable to updated") }
+
+    fun deleteTodo(todo: Todo) = todosCollectionRef.document(todo.id).delete()
+
+    suspend fun getTodoListByProject(pid: String) = todosCollectionRef
+        .whereEqualTo("pid", pid)
+        .get().await().toObjects(Todo::class.java)
 }
